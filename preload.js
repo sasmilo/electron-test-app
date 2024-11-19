9972a46b971e2ca4c3cdb518e33cee084c6db6db
nbsgmbh <sasa.milosevic@nbs.co.at>
2024-11-19 14:18:46 +0100

    Handle data exchange front to back

--- preload.js
+++ preload.js
@@ -3,6 +3,8 @@ const { contextBridge } = require('electron')
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron
+  electron: () => process.versions.electron,
+  ping: () => ipcRenderer.invoke('ping')
+
   // we can also expose variables, not just functions
 })
